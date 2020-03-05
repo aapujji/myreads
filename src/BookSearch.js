9a6a5974e476acb3a816c61e64e3b855fd3362eb
apujji <apujji@datalot.com>
2020-03-04 21:13:53 -0500

    Move search logic to separate component

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -1,14 +1,80 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 import Book from './Book'
+import * as BooksAPI from './BooksAPI'
+import { Link } from 'react-router-dom'
 
 class BookSearch extends React.Component {
+	state = {
+		query: '',
+		error: false,
+		filteredBooks: []
+	}
+
+	filterBooks = (query) => {
+		this.setState({ query })
+
+		if (!query || query === '') {
+			this.setState({
+				filteredBooks: [],
+				error: true
+			})
+		} else {
+			BooksAPI.search(query)
+				.then(results => {
+					results.map((result) => {
+						for (const bookSaved of this.props.booksSaved) {
+							if (result.id === bookSaved.id) {
+								result.shelf = bookSaved.shelf
+							}
+						}
+					})
+					this.setState({
+						filteredBooks: results,
+						error: false
+					})
+				})
+				.catch((err) => {
+					this.setState({
+						filteredBooks: [],
+						error: true
+					})
+				})
+		}
+	}
+
 	render() {
-		const { book, updateShelf } = this.props;
+		const { booksSaved, updateShelf } = this.props;
+		const { query, error, filteredBooks } = this.state;
 
 		return (
-			<Book book={book} updateShelf={updateShelf} />
+			<div className="search-books">
+				<div className="search-books-bar">
+					<Link to="/" className="close-search">Close</Link>
+					<div className="search-books-input-wrapper">
+						<input type="text" placeholder="Search by title or author" value={query} onChange={(e) => this.filterBooks(e.target.value)} />
+					</div>
+				</div>
+				<div className="search-books-results">
+					<ol className="books-grid">
+						{error === true && (
+							<div>Sorry, there are no books that match your search</div>
+						)}
+						{error === false && (
+							filteredBooks.map((book) => (
+								<Book key={book.id} book={book} shelf={book.shelf} updateShelf={updateShelf} />
+							))
+						)}
+					</ol>			
+				</div>
+			</div>
 		)
 	}
 }
+
+BookSearch.propTypes = {
+	booksSaved: PropTypes.array.isRequired,
+	updateShelf: PropTypes.func.isRequired
+};
 
 export default BookSearch
